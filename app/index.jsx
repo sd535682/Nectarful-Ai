76cf3a6319f8b07fc8ab4aff800136ca63048848
Subhadeep Das <sd535682@gmail.com>
2024-07-20 02:51:04 +0530

    Basic Setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import Home from "../components/home";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Reset from Scratch</Text>
+      <Home />
     </View>
   );
 }
