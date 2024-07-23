f4bdf06e22b0a14b20b487cefa947b4049696562
Subhadeep Das <sd535682@gmail.com>
2024-07-23 20:10:42 +0530

    responsive design

--- components/cards.jsx
+++ components/cards.jsx
@@ -1,10 +1,16 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Image } from "expo-image";
+import { hp, wp } from "../constants/responsive";
 
-const Cards = ({ item }) => {
+const Cards = ({ item, index, column }) => {
+  const isLast = () => {
+    return (index + 1) % column === 0;
+  };
   return (
-    <Pressable onPress={{}} style={styles.card_container}>
+    <Pressable
+      style={[styles.card_container, !isLast() && styles.card_spacing]}
+    >
       <Text style={styles.card_text}>{item}</Text>
       <Image
         source={require("../assets/images/blackberry.png")}
@@ -21,17 +27,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "pink",
-    marginBottom: 10,
-    borderRadius: 20,
-    marginHorizontal: 5,
+    marginBottom: wp(2),
+    borderRadius: wp(4),
     padding: 10,
   },
   card_text: {
     fontWeight: "bold",
   },
   card_image: {
-    height: 200,
+    height: hp(25),
     width: "100%",
     contentFit: "cover",
+  },
+  card_spacing: {
+    marginRight: wp(2),
   },
 });
