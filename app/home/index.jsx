410e891c908d3e14bef43885b769baca7b6d0385
Subhadeep Das <sd535682@gmail.com>
2024-07-29 22:15:43 +0530

    axios implementation

--- app/home/index.jsx
+++ app/home/index.jsx
@@ -1,18 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 import ListView from "../../components/list";
-import Header from "../../components/header";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Search from "../../components/search";
 import { hp, wp } from "@/constants/responsive";
 import { StatusBar } from "expo-status-bar";
+import { Feather } from "@expo/vector-icons";
+import { Image } from "expo-image";
 
 const Home = () => {
   const { top } = useSafeAreaInsets();
   const paddingTop = top > 0 ? top + 10 : top + 30;
+
   return (
     <View style={[styles.home_container, { paddingTop }]}>
-      <Header />
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/images/avatar.png")}
+          style={styles.avatar}
+          contentFit="contain"
+        />
+        <Pressable style={styles.cart}>
+          <Feather name="shopping-bag" size={hp(3)} color="grey" />
+        </Pressable>
+      </View>
       <Text style={styles.home_title}>Explore Fruits</Text>
       <Search />
       <ListView />
@@ -33,7 +44,28 @@ const styles = StyleSheet.create({
     paddingVertical: wp(2),
     paddingHorizontal: wp(2),
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: wp(2),
+  },
+  avatar: {
+    width: hp(5.5),
+    height: hp(5.5),
+    borderRadius: 100,
+  },
+  cart: {
+    borderRadius: 15,
+    padding: hp(1),
+    borderColor: "lightgrey",
+    borderWidth: 2,
+  },
 });
 
+// . Infos
+
+// ***** For top padding on different devices according notches *****
+//
 //   const { top } = useSafeAreaInsets();
 //   const paddingTop = top > 0 ? top + 10 : top + 30;
