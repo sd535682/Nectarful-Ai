dd92f053fde28f9a18b24460fe3b83be8db34887
Subhadeep Das <sd535682@gmail.com>
2024-07-31 00:56:37 +0530

    splash screen + icon + auth screen

---
+++ components/authinput.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AuthInput = () => {
+  return (
+    <View>
+      <Text>AuthInput</Text>
+    </View>
+  );
+};
+
+export default AuthInput;
+
+const styles = StyleSheet.create({});
