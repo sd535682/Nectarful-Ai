0c307a85ed0d141beddd1cac5a7e51e78452920b
Subhadeep Das <sd535682@gmail.com>
2024-08-06 00:12:00 +0530

    recipe generation completed

--- app/(home)/smoothie/index.jsx
+++ app/(home)/smoothie/index.jsx
@@ -3,12 +3,17 @@ import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useSmoothieItems } from "../../../zustand/fruitcart";
 import { hp, wp } from "../../../constants/responsive";
-import { Image } from "expo-image";
+import { ScrollView } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useEffect } from "react";
+import { getSmoothieRecipe } from "../../../api/globalapi";
+import { useState } from "react";
+import Markdown from "react-native-markdown-display";
 
 const Smoothie = () => {
   const { top } = useSafeAreaInsets();
+  const [recipe, setRecipe] = useState("");
   const paddingTop = top > 0 ? top + 10 : top + 30;
   const { smoothieItems, removeFruit, clearSmoothieItems } = useSmoothieItems(
     (state) => ({
@@ -19,25 +24,78 @@ const Smoothie = () => {
     })
   );
 
-  // console.log(smoothieItems[fruit.name].imageurl)
-
+  // Smoothie Call
   return (
     <View style={[styles.smoothie_page, { paddingTop }]}>
-      <View style={styles.smoothie_header}>
-        <Text style={styles.smoothie_title}>Smoothie Recipe</Text>
-        <Pressable onPress={clearSmoothieItems}>
-          <FontAwesome name="remove" size={30} color="red" />
-        </Pressable>
-      </View>
-      {smoothieItems.length ? smoothieItems.map((fruit) => (
-        <View key={fruit.id} style={styles.recipe_item}>
-          <Image source={fruit.imageurl} style={{ height: 50, width: 50 }} />
-          <Text style={styles.smoothieItemText}>{fruit.name}</Text>
-          <Pressable onPress={() => removeFruit(fruit.id)}>
-            <MaterialIcons name="delete-outline" size={24} color="black" />
+        <View style={styles.smoothie_header}>
+          <Text style={styles.smoothie_title}>Smoothie Recipe</Text>
+          <Pressable
+            onPress={() => {
+              clearSmoothieItems(), setRecipe("");
+            }}
+            style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+          >
+            <Text style={{ fontWeight: "bold" }}>Clear All</Text>
+            <FontAwesome name="remove" size={30} color="red" />
           </Pressable>
         </View>
-      )): <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}><Text style={styles.smoothieItemText}>Jar looks empty {'\n'} 🥤 Add Items</Text></View>}
+        <View style={{ flex: 0.1, flexDirection: "row", alignItems: "center" }}>
+          <ScrollView
+            horizontal
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {smoothieItems.length ? (
+              smoothieItems.map((fruit) => (
+                <View key={fruit.id} style={styles.recipe_item}>
+                  <Text style={styles.smoothieItemText}>{fruit.name}</Text>
+                  <Pressable onPress={() => removeFruit(fruit.id)}>
+                    <MaterialIcons
+                      name="delete-outline"
+                      size={20}
+                      color="red"
+                    />
+                  </Pressable>
+                </View>
+              ))
+            ) : (
+              <View
+                style={{ flexDirection: "row", textAlign: "center", flex: 1 }}
+              >
+                <Text style={styles.smoothieItemText}>
+                  Jar looks empty 🥤 Add Items
+                </Text>
+              </View>
+            )}
+          </ScrollView>
+        </View>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Pressable
+            style={{
+              backgroundColor: "black",
+              borderRadius: 15,
+              paddingHorizontal: 20,
+              paddingVertical: 10,
+            }}
+            onPress={() =>
+              getSmoothieRecipe(smoothieItems)
+                .then((recipe) => setRecipe(recipe))
+                .catch((error) => console.error("Error:", error))
+            }
+          >
+            <Text style={{ fontSize: 15, fontWeight: "bold", color: "white", }}>
+              Generate Smoothie
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{flex: 1, marginBottom: hp(10)}}>
+          <ScrollView>
+          <Markdown style={{body:{fontSize: 15, color: '#411020'}}} mergeStyle='false'>{recipe}</Markdown>
+          </ScrollView>
+        </View>
     </View>
   );
 };
@@ -52,7 +110,7 @@ const styles = StyleSheet.create({
     fontWeight: "900",
   },
   smoothie_header: {
-    flexDirection: 'row',
+    flexDirection: "row",
     justifyContent: "space-between",
     marginBottom: 10,
   },
@@ -63,13 +121,13 @@ const styles = StyleSheet.create({
     borderColor: "black",
     marginBottom: 10,
     borderRadius: 15,
-    alignItems: "center",
-    justifyContent: "space-between",
     paddingHorizontal: 10,
     paddingVertical: 5,
+    height: hp(5),
+    marginRight: 10,
   },
   smoothieItemText: {
-    fontSize: hp(2.5),
+    fontSize: hp(2),
     fontWeight: "bold",
   },
 });
--- zustand/fruitcart.jsx
+++ zustand/fruitcart.jsx
@@ -2,7 +2,13 @@ import { create } from "zustand";
 
 export const useSmoothieItems = create((set) => ({
     smoothieItems: [],
-    addFruit: (fruit) => set(state => ({ smoothieItems: state.smoothieItems.find(item => item.id === fruit.id)? state.smoothieItems : [...state.smoothieItems, fruit] })),
+    addFruit: (fruit) => set(
+        state => {
+            if(state.smoothieItems.length >= 5) {return state}
+            return state.smoothieItems.find(item => item.id === fruit.id)? {smoothieItems: state.smoothieItems} : {smoothieItems : [...state.smoothieItems, fruit]}
+        }
+    ),
+    // addFruit: (fruit) => set(state => ({ smoothieItems: state.smoothieItems.find(item => item.id === fruit.id)? state.smoothieItems : [...state.smoothieItems, fruit] })),
     removeFruit: (fruitId) => set(state => ({ smoothieItems: state.smoothieItems.filter(item => item.id!== fruitId) })),
     clearSmoothieItems: () => set({ smoothieItems: [] }),
 }));
