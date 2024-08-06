9f556571fd1fbd96e3b71601e6c36b911cfb36ed
Subhadeep Das <sd535682@gmail.com>
2024-08-06 21:24:38 +0530

    profile add + fix api call loop

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -1,12 +1,14 @@
 import { Tabs } from "expo-router";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function HomeLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false , tabBarStyle: {backgroundColor: '#e11d48', position: "absolute", bottom: 10, left: 10, right: 10, borderRadius: 15, height: 60}}}>
       <Tabs.Screen name="index" options={{title: 'Fruits', tabBarShowLabel: false, tabBarIcon: () => <MaterialCommunityIcons name="fruit-watermelon" size={30} color='white'/>}}/>
       <Tabs.Screen name= 'smoothie/index' options={{title: 'Smoothie', tabBarShowLabel: false, tabBarIcon: ()=> <FontAwesome6 name="jar-wheat" size={30} color="white" />}} />
+      <Tabs.Screen name= 'profile/index' options={{title: 'Profile', tabBarShowLabel: false, tabBarIcon: ()=> <FontAwesome name="user" size={30} color="white" />}} />
     </Tabs>
   );
 }
